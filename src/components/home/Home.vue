d992f8165889766e84e5a4fd663528243f0f4975
wilsonLWX <15000108650@163.com>
2021-02-28 11:44:07 +0800

    header

---
+++ src/components/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <!-- 三种写法都可以 -->
+      <!-- <HomeHeader></HomeHeader> -->
+      <!-- <HomeHeader /> -->
+      <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './pages/Header'
+export default {
+    components:{
+        HomeHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//js 取消延迟300毫秒触发事件
+import FastClick from 'fastclick'
+FastClick.attach(document.body)
+
+//css
+import './assets/css/reset.css'
+import './assets/css/iconfont.css'
+
 Vue.config.productionTip = false
 
 new Vue({
